0a79a29d316dd80cd17f0c4be3ac68502de4aca6
Manex Aranzadi <manexae@protonmail.com>
2026-01-31 22:24:10 +0100

    no se ni que he echo

--- shot/merge.c
+++ shot/merge.c
@@ -12,8 +12,13 @@ int main(int argc, char const *argv[])
     int myNumbers[] = {6,3,4,1,5,2,7,0};
     int size = sizeof(myNumbers) / sizeof(myNumbers[0]);
 
-    ordenar(myNumbers, size);
+    int ordenatuta[] = ordenar(myNumbers, size);
 
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("El número es: %d\n", ordenatuta[i]);
+    }
+    
 
 
     return 0;
@@ -22,22 +27,54 @@ int main(int argc, char const *argv[])
 int ordenar(int *lista, int size){
     //int listaOr[size];
 
+    int numbers[size];
+    
+    int mid = size / 2;
+
+    int left[mid];
+    int right[mid];
+
+
+    for (int i = 0; i < mid; i++) {
+        left[i] = lista[i];
+        right[i] = lista[mid + i];
+    }
+
     if (size !=1)
     {
-        int mid = size / 2;
-
-        int left[mid];
-        int right[mid];
-
-
-        for (int i = 0; i < mid; i++) {
-            left[i] = lista[i];
-            right[i] = lista[mid + i];
-        }
+        
         ordenar(left, mid);
         ordenar(right, mid);
     }else{
-        printf("El número es: %d\n", lista[0]);
+        int leftSize =0;
+        int rigthSize = 0;
+        int indi=0;
+        while (leftSize<mid && rigthSize<mid)
+        {
+            if (leftSize>mid)
+            {
+                numbers[indi] = right[rigthSize];
+                rigthSize++;
+            }
+            else if (rigthSize>mid)
+            {
+                numbers[indi] = left[leftSize];
+                leftSize++;
+            }
+            else if (left[leftSize]> right[rigthSize])
+            {
+                numbers[indi] = right[rigthSize];;
+                rigthSize++;
+            }else if (left[leftSize]< right[rigthSize])
+            {
+                numbers[indi] = left[leftSize];;
+                leftSize++;
+            }
+            indi++;
+        }
+        
+        return numbers;
+        
     }
 
     
